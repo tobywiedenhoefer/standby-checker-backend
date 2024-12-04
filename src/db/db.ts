f5b342d020cb3a31ce4c1479d7dc6b36916cfa66
tobywiedenhoefer <toby.wiedenh@gmail.com>
2024-12-03 19:40:00 -0600

    Migrated from local db to Supabase

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,17 +1,24 @@
-import Bun from "bun"
+import Bun from "bun";
 
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { Client } from 'pg';
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Client } from "pg";
 
-
-const client = new Client({
+// on-prem db
+/* const client = new Client({
     host: Bun.env.DB_HOST,
     port: Number.parseInt(Bun.env.DB_PORT ?? "5432"),
     user: Bun.env.DB_USER,
     password: Bun.env.DB_PASS,
     database: Bun.env.DB_NAME
-})
+}) */
 
-client.connect().then(() => console.log("successfully connected")).catch((e) => console.log("failure: ", e))
+const client = new Client(Bun.env.DB_URI ?? "");
 
-export default drizzle(client)
+client
+  .connect()
+  .then(() => console.log("successfully connected to db"))
+  .catch(() => {
+    console.log("could not connect to db");
+  });
+
+export default drizzle(client);
