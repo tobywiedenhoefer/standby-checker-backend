cee33fd1f85b4b042bf69e6406f98c94cf4c00f8
tobywiedenhoefer <toby.wiedenh@gmail.com>
2024-12-11 21:37:20 -0600

    Updated destination db calls to use queries

--- src/routes/destinationRoutes.ts
+++ src/routes/destinationRoutes.ts
@@ -25,35 +25,21 @@ router.get("/search?searchText=:searchText", async (req, res) => {
   let resp: ApiResponse<Destination[]>;
   try {
     const query = await Destinations.get(searchText);
-    const destinations: { [destId: string]: Destination } = {};
-    query.forEach((row) => {
-      if (!destinations[row.destinations.id]) {
-        destinations[row.destinations.id] = {
-          ...row.destinations,
-          airports: row.airports ? [{ ...row.airports }] : [],
-        };
-      } else if (
-        row.airports &&
-        !destinations[row.destinations.id].airports
-          .map((a) => a.id)
-          .includes(row.airports.id)
-      ) {
-        destinations[row.destinations.id].airports.push({ ...row.airports });
-      }
-      resp = {
-        success: true,
-        payload: Object.values(destinations),
-      };
-      res.status(200).json(resp)
-    });
+    resp = {
+      success: true,
+      payload: query.map((row) => ({
+        ...row,
+      })),
+    };
+    res.status(200).json(resp);
   } catch (e) {
     resp = {
       success: false,
       errorCode: 1,
       errorMessage: `Error while getting destinations: ${e}`,
     };
-    res.status(500).json(resp)
+    res.status(500).json(resp);
   }
 });
 
-export { router as destinationRoutes }
+export { router as destinationRoutes };
--- src/services/db/destinations.ts
+++ src/services/db/destinations.ts
@@ -1,32 +1,26 @@
-import { eq, like, or } from "drizzle-orm";
-
 import db from "@/db/db";
-import { destinationsTable, airportsTable } from "@/db/schema";
 
 export const get = async (searchText?: string) => {
   /**
    * Using an optional `searchText` string, get a list of flight destinations.
    * Response: {
-   *
+   *   destinations: DestinationRows;
+   *   airports: AirportRows | null;
    * }
    */
   searchText = searchText ?? "";
-  const query = await db
-    .select()
-    .from(destinationsTable)
-    .leftJoin(
-      airportsTable,
-      eq(destinationsTable.id, airportsTable.destinationId)
-    )
-    .where(
-      or(
-        like(destinationsTable.city, searchText),
-        like(destinationsTable.country, searchText),
-        like(destinationsTable.state, searchText),
-        like(airportsTable.code, searchText),
-        like(airportsTable.name, searchText)
-      )
-    )
-    .limit(50);
+  const query = await db.query.destinations.findMany({
+    with: {
+      airports: true,
+    },
+    where: (destinations, { like, or }) => {
+      return or(
+        like(destinations.city, searchText),
+        like(destinations.country, searchText),
+        like(destinations.state, searchText)
+      );
+    },
+    limit: 50,
+  });
   return query;
 };
