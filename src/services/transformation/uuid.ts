acc63c47f3b9fd4eaba8ef1d250434a878a6c4b2
tobywiedenhoefer <toby.wiedenh@gmail.com>
2024-12-15 11:58:40 -0600

    Added ability to generate random UUID without a seed

--- src/services/transformation/uuid.ts
+++ src/services/transformation/uuid.ts
@@ -1,7 +1,10 @@
 import Bun from "bun";
-import { v5 as uuid } from "uuid";
+import { v5 as uuid, v4 as generateRandomUUID } from "uuid";
 
-export const get = (str: string | Uint8Array, ns?: string) => {
+export const get = (str?: string | Uint8Array, ns?: string) => {
+  if (typeof str === "undefined") {
+    return generateRandomUUID();
+  }
   const namespace = ns ?? Bun.env.NAMESPACE;
   if (!namespace) {
     throw Error(
