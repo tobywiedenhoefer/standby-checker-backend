8bcab3068b90919af3f694ede4cc94b8f0ebea2f
tobywiedenhoefer <toby.wiedenh@gmail.com>
2024-12-14 23:46:27 -0600

    Wrapped flights query in a try catch statement

--- src/services/db/flights.ts
+++ src/services/db/flights.ts
@@ -2,28 +2,42 @@ import db from "@/db/db";
 import SearchFilters from "@/types/db/SearchFilter";
 
 export const get = async (filters: SearchFilters) => {
-  const prepared = await db.query.flights
-    .findMany({
-      where: (flights, { eq, or, sql }) =>
-        or(
-          eq(flights.airlineId, sql.placeholder("airlineId")),
-          eq(sql.placeholder("airlineId"), "")
-        ),
-      with: {
-        from: true,
-        to: true,
-        airline: {
-          with: {
-            alliance: true,
+  try {
+    const prepared = await db.query.flights
+      .findMany({
+        where: (flights, { eq, or, sql }) =>
+          or(
+            eq(flights.airlineId, sql.placeholder("airlineId")),
+            eq(sql.placeholder("airlineId"), "")
+          ),
+        with: {
+          from: {
+            with: {
+              airports: true,
+            },
           },
+          to: {
+            with: {
+              airports: true,
+            },
+          },
+          airline: {
+            with: {
+              alliance: true,
+            },
+          },
+          fromAirport: true,
+          toAirport: true,
         },
-      },
-    })
-    .prepare("flights_query");
-  const flights = await prepared.execute({
-    airlineId: filters.airlineIds ?? "",
-  });
-  return !filters.allianceId
-    ? flights
-    : flights.filter((row) => row.airline.allianceId === filters.allianceId);
+      })
+      .prepare("flights_query");
+    const flights = await prepared.execute({
+      airlineId: filters.airlineIds ?? "",
+    });
+    return !filters.allianceId
+      ? flights
+      : flights.filter((row) => row.airline.allianceId === filters.allianceId);
+  } catch (e) {
+    throw new Error(`Error thrown while querying flights: ${e}`);
+  }
 };
